bebc0574dca4bf328147044483fae33c12db0236
jianchengwu <jianchengw26@gmail.com>
2017-04-03 19:27:57 -0700

    enable clickedEffect

---
+++ object/clickedEffect.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wuji0068 on 4/3/2017.
+ */
+
+function clickedEffect(x,y,ctx,size){
+    ctx.beginPath();
+    ctx.rect(x,y,size,size);
+    ctx.fillStyle = "#DEB887";
+    ctx.fill();
+}
